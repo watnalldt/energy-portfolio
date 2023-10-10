8d92ddb9b5b0dc3a9ba92b7b1459439a34ebe3c1
watnalldt <davidaturner56@yahoo.com>
2023-10-10 09:32:02 +0100

    Configure debug toolbar

--- config/settings.py
+++ config/settings.py
@@ -23,6 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 env = environ.Env(
     ALLOWED_HOSTS=(list, []),
     DEBUG=(bool, False),
+    DEBUG_TOOLBAR=(bool, False),
 )
 env.read_env(str(BASE_DIR / ".env.development"))
 
@@ -30,6 +31,7 @@ SECRET_KEY = env("SECRET_KEY")
 
 ALLOWED_HOSTS: list[str] = env("ALLOWED_HOSTS")
 DEBUG = env("DEBUG")
+DEBUG_TOOLBAR = env("DEBUG_TOOLBAR")
 
 
 # Application definition
@@ -88,6 +90,13 @@ MIDDLEWARE = [
     "axes.middleware.AxesMiddleware",  # Axes
     "django_auto_logout.middleware.auto_logout",  # Auto-logout
 ]
+
+# Enable the debug toolbar only in DEBUG mode.
+if DEBUG and DEBUG_TOOLBAR:
+    INSTALLED_APPS.append("debug_toolbar")
+    MIDDLEWARE.insert(0, "debug_toolbar.middleware.DebugToolbarMiddleware")
+    INTERNAL_IPS = ["127.0.0.1"]
+
 AUTH_USER_MODEL = "users.User"
 LOGOUT_REDIRECT_URL = "users:login"
 ROOT_URLCONF = "config.urls"
--- users/templatetags/client_tags.py
+++ users/templatetags/client_tags.py
@@ -1,9 +1,21 @@
+from datetime import date, timedelta
+
 from django import template
+from django.db.models import Q
+from django.utils import timezone
 
 from clients.models import Client
 from contracts.models import Contract
 
 register = template.Library()
+
+# Get the current date
+current_date = timezone.now()
+
+# Calculate the end dates for 30, 60, and 90 days from the current date
+end_date_30_days = current_date + timedelta(days=30)
+end_date_60_days = current_date + timedelta(days=60)
+end_date_90_days = current_date + timedelta(days=90)
 
 
 @register.simple_tag
@@ -88,4 +100,14 @@ def total_contracts_end_dates(user):
 def total_contracts_ooc(user):
     """Returns most 5 most recent events"""
     contracts = Contract.objects.filter(client__account_manager=user).filter(is_ooc="YES").count()
+    return contracts
+
+
+@register.simple_tag
+def contract_30_days(user):
+    contracts = (
+        Contract.objects.filter(client__account_manager=user)
+        .filter(contract_end_date__lt=end_date_30_days)
+        .count()
+    )
     return contracts
